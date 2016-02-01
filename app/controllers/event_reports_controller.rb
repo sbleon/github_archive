42457c83ae4b4c122461e591b34482c9a1d7644d
Leon Miller-Out <leon@singlebrook.com>
2016-01-31 20:26:00 -0500

    Fix a couple of bugs when first loading #index

--- app/controllers/event_reports_controller.rb
+++ app/controllers/event_reports_controller.rb
@@ -6,7 +6,8 @@ class EventReportsController < ApplicationController
     params[:page] ||= 1
 
     @event_types = EventReports::IndexQuery.event_types
-    @query = EventReports::IndexQuery.new(params[:event_reports_index_query])
+    index_params = params[:event_reports_index_query] || {}
+    @query = EventReports::IndexQuery.new(index_params)
     @count = @query.result.count
     @event_reports = @query.result.page(params[:page]).load
     @time_elapsed = (1000.0 * (Time.current - start_time)).to_i
